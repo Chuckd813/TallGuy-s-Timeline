1896731f3a6acd60c6b3a530ff238839c96a20ee
charles <charles@mediavii.com>
2025-06-03 13:42:47 +0000

    New Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="apple-touch-icon" href="/images/tg192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/images/tallguy.jpg" type="image/jpeg" sizes="any" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
